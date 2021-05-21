f947ae3d1e16f1a6eece2b230bde1aa7a1eede79
Alejo boga <alejoboga20@gmail.com>
2021-05-21 10:16:07 -0300

    Editing response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,24 @@
 const { response } = require('express');
 
 const createUser = (req, res = response) => {
-  console.log(req.body);
-  res.json({ ok: true, msg: 'new' });
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'Name should have more than 5 letters'
+    });
+  }
+
+  res.json({ ok: true, msg: 'new', name, email, password });
 };
 
-const loginUser = (req, res) => {
-  res.json({ ok: true, msg: 'login' });
+const loginUser = (req, res = response) => {
+  const { email, password } = req.body;
+  res.json({ ok: true, msg: 'login', email, password });
 };
 
-const renewToken = (req, res) => {
+const renewToken = (req, res = response) => {
   res.json({ ok: true, msg: 'renew' });
 };
 
