c27b2e9b1007b63351cab513c0cde2837ee04f44
Alejo boga <alejoboga20@gmail.com>
2021-05-21 09:23:53 -0300

    configuring public directory

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, rest) => {});
+app.use(express.static('public'));
 
 app.listen(4000, () => console.log('server up'));
