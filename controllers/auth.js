f5badd5871b1072adbb24a132c69c61222f1282d
Alejo boga <alejoboga20@gmail.com>
2021-05-25 15:42:33 -0300

    Adding validation before hit DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,17 @@ const User = require('../models/User');
 
 const createUser = async (req, res = response) => {
   const { name, email, password } = req.body;
-  const user = new User(req.body);
 
   try {
-    await user.save();
-    res.status(201).json({ ok: true, msg: 'new', name, email, password });
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(404).json({ ok: false, msg: 'User already exits' });
+    } else {
+      user = new User(req.body);
+      await user.save();
+      return res.status(201).json({ ok: true, msg: 'new', name, email, password });
+    }
   } catch (error) {
     res.status(500).json({ ok: false, msg: 'User credentials invalid' });
   }
