073271f246e3609e0ae9433803d5018d9b36ee95
Alejo boga <alejoboga20@gmail.com>
2021-05-25 15:33:46 -0300

    Adding user creation in controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,17 @@
 const { response } = require('express');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
+const createUser = async (req, res = response) => {
   const { name, email, password } = req.body;
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+
   res.status(201).json({ ok: true, msg: 'new', name, email, password });
 };
 
