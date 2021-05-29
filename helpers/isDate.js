95581bfcd3cd42a481caf36f7d66af9342c3ca8d
Alejo Boga <alejoboga20@gmail.com>
2021-05-29 10:05:38 -0300

    Adding custom validator

---
+++ helpers/isDate.js
@@ -0,0 +1,17 @@
+const moment = require('moment');
+
+const isDate = (value, { req, location, path }) => {
+  if (!value) {
+    return false;
+  }
+
+  const date = moment(value);
+
+  if (date.isValid()) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = { isDate };
