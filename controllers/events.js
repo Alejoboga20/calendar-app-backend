6f163d165c113d0d321bfcd7e41e3bb0cc7d684a
Alejo Boga <alejoboga20@gmail.com>
2021-05-31 09:37:22 -0300

    Creating getEvents controller

--- controllers/events.js
+++ controllers/events.js
@@ -2,9 +2,11 @@ const { response } = require('express');
 const Event = require('../models/Event');
 
 const getEvents = async (req, res = response) => {
+  const events = await Event.find().populate('user', 'name');
+
   res.json({
     ok: true,
-    msg: 'getEvents'
+    events: events
   });
 };
 
